c933bd360556188c77fc184e3f89e7440bff4a00
Aritra Kar <aritrakar2002@gmail.com>
2020-08-27 18:53:41 +0530

    Updates

---
+++ precache-manifest.b3072e8e9953b5b7bede41d0a6c98682.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cd5a8d51811feb00c00e1473f1a19f",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "493d48d504e9af2ea899",
+    "url": "/todo/static/css/main.4a913632.chunk.css"
+  },
+  {
+    "revision": "9653048a15b184c98bbf",
+    "url": "/todo/static/js/2.d060ad9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo/static/js/2.d060ad9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "493d48d504e9af2ea899",
+    "url": "/todo/static/js/main.29f31ce2.chunk.js"
+  },
+  {
+    "revision": "da7c3f7f7e5963fe74b8",
+    "url": "/todo/static/js/runtime-main.74315f96.js"
+  }
+]);
