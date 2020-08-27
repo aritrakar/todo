43ee8a68cf3967fa68e2a63668261fc7a6ddead9
Aritra Kar <aritrakar2002@gmail.com>
2020-08-27 19:30:07 +0530

    Modified index.html

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import './App.css';
-import TDC from "./ToDoComponent";
+import TDC from "./ToDoComponent.jsx";
 import todosData from "./todosData.js";
 
 let todoArray = [];
