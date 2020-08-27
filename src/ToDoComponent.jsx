2a71ddcbcb668393babd085264892841edcb03c1
Aritra Kar <aritrakar2002@gmail.com>
2020-08-27 18:30:28 +0530

    First commit

---
+++ src/ToDoComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function TDC(props) {
+  const styles = {
+    textDecoration: "line-through",
+    color: "#d9d3d2",
+    fontStyle: "italic",
+  };
+  return (
+    <div className="todo-item">
+      <input
+        type="checkbox"
+        checked={props.item.completed}
+        onChange={() => props.handleChange(props.item.id)}
+      />
+      <p style={props.item.completed ? styles : null}>{props.item.text}</p>
+    </div>
+  );
+}
